f42a0ebb31ac47053f745790bbb8ffaedfd5b46f
Fernando Futila <fernando.futila@gmail.com>
2023-02-16 15:35:16 -0300

    Created Login page

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -1,7 +1,84 @@
+import { useState } from "react";
 import PageLayout from "@/src/components/Layout";
 
+import {
+  FormContainer,
+  ViewContainer,
+  FormSubtitle,
+  FormTitle,
+  ButtonEnter,
+  FormQuestion,
+  FormQuestionAction,
+  FormErrorMsg,
+} from "./styles";
+
+import { TextField } from "@mui/material";
+
+interface UserLoginType {
+  email: string;
+  password: string;
+}
+
 const LoginView = () => {
-  return <PageLayout title="Entrar">Alguma coisa</PageLayout>;
+  const [loginData, setLoginData] = useState<UserLoginType>({
+    email: "",
+    password: "",
+  });
+
+  const [msgError, setMessageError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    setLoginData({
+      ...loginData,
+      [target.name]: target.value,
+    });
+
+    setMessageError("");
+  };
+
+  const handleLogin = () => {
+    if (loginData.email === "" && loginData.password === "") {
+      setMessageError("Os campos são obrigatórios");
+    } else {
+      //
+    }
+  };
+
+  return (
+    <PageLayout title="Entrar">
+      <ViewContainer>
+        <FormContainer>
+          <FormTitle>Entrar</FormTitle>
+          <FormSubtitle>Entre e arrenda uma bicicleta hoje</FormSubtitle>
+          <TextField
+            onChange={handleChange}
+            name="email"
+            className="ipt-form"
+            placeholder="Email"
+          />
+          <TextField
+            onChange={handleChange}
+            name="password"
+            className="ipt-form"
+            placeholder="Password"
+          />
+
+          {!!msgError && <FormErrorMsg>{msgError}</FormErrorMsg>}
+
+          <ButtonEnter disabled={loading} onClick={handleLogin}>
+            {loading ? "carregando" : "Entrar"}
+          </ButtonEnter>
+
+          <div className="question-container">
+            <FormQuestion>Não possui uma conta ?</FormQuestion>
+            <FormQuestionAction>Registar</FormQuestionAction>
+          </div>
+        </FormContainer>
+      </ViewContainer>
+    </PageLayout>
+  );
 };
 
 export default LoginView;
---
+++ src/views/LoginView/styles.ts
@@ -0,0 +1,100 @@
+import styled from "@emotion/styled";
+
+export const ViewContainer = styled.div`
+  width: 100%;
+  height: auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 100px;
+`;
+
+export const FormContainer = styled.div`
+  width: auto;
+  height: auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .ipt-form {
+    margin-top: 20px;
+    height: 50px;
+    width: 275px;
+  }
+
+  .question-container {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+  }
+`;
+
+export const FormTitle = styled.h1`
+  font-family: "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 45px;
+  line-height: 55px;
+  color: #e6474b;
+`;
+
+export const FormSubtitle = styled.p`
+  font-family: "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 34px;
+
+  color: #000000;
+`;
+
+export const ButtonEnter = styled.button`
+  width: 215px;
+  height: 40px;
+  background: #e6474b;
+  border-radius: 6px;
+  margin-top: 30px;
+  outline: none;
+  border: none;
+
+  font-family: "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  text-transform: uppercase;
+
+  color: #ffffff;
+
+  cursor: pointer;
+`;
+
+export const FormQuestion = styled.p`
+  font-family: "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+
+  color: #000000;
+`;
+
+export const FormQuestionAction = styled(FormQuestion)`
+  text-decoration-line: underline;
+  color: #e6474b;
+  margin-left: 5px;
+`;
+
+export const FormErrorMsg = styled.p`
+  font-family: "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 17px;
+
+  color: #e6474b;
+  margin-top: 20px;
+`;
